f493c7624ec0009381b3814b796ac6e9b9a2a152
Dailton-Mac <dailtonbastos@gmail.com>
2019-06-20 23:56:41 -0300

    Criado as rotas para os controllers lending, users_fields, users_values

--- src/routes.js
+++ src/routes.js
@@ -11,17 +11,18 @@ routes.get('/', (req, res) => {
 routes.get('/requests', controllers.RequestsController.list)
 routes.post('/requests/new', controllers.RequestsController.store)
 
-// User Routes
+// User Routes / Usuário
 routes.get('/users', controllers.UserController.list)
+routes.get('/user/values', controllers.UserController.listUsersValues)
 routes.post('/user/new', controllers.UserController.store)
 
-// Users Types
+// Users Types / Tipos de Usuários
 routes.get('/user/types', controllers.UsersTypesController.listUsersTypes)
 
-// Users Fields
+// Users Fields / Complementos cadastro usuário
 routes.get('/user/fields', controllers.UsersFieldsController.listUsersFields) // Erro não encontra o ID
 
-// Biblio Records
+// Biblio Records / Cadastro de Livro
 routes.get('/biblio/records', controllers.BiblioAllController.listBiblioRecords)
 
 routes.post(
@@ -29,11 +30,27 @@ routes.post(
   controllers.BiblioAllController.storeBiblioRecords
 )
 
-// Reservations
+// Reservations / Reservas
 routes.get('/reservations', controllers.ReservationsController.listReservations)
 routes.post(
   '/reservation/new',
   controllers.ReservationsController.reservationCreate
 )
 
+// Lendings / Emprestimos
+
+routes.get('/lendings', controllers.LendingController.listLendings)
+routes.post('/lending/new', controllers.LendingController.createLending)
+
+// Biblio_Holdings / Exemplares
+
+routes.get(
+  '/biblio/holdings',
+  controllers.BiblioAllController.listBiblioHoldings
+)
+
+routes.post(
+  '/biblio/holding/new',
+  controllers.BiblioAllController.storeBiblioHoldings
+)
 module.exports = routes
